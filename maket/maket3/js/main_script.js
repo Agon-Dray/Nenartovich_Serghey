4a066ec9e999e37604bae559f68e5c069b22f735
Agon-Dray <argodray@gmail.com>
2023-06-21 23:29:53 +0300

    notcomplit

---
+++ maket/maket3/js/main_script.js
@@ -0,0 +1,104 @@
+var series = [
+    {
+        bgImgUrl: "../img/series/1.png",
+        seriesName: "Wu Assassins",
+        actor: "Iko Uwais",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/fox.png"
+    },
+    {
+        bgImgUrl: "../img/series/2.png",
+        seriesName: "Project Adam",
+        actor: "Iko Uwais",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/abc.png"
+    },
+    {
+        bgImgUrl: "../img/series/3.png",
+        seriesName: "Monster Truck",
+        actor: "Emili Watson",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/nbc.png"
+    },
+    {
+        bgImgUrl: "../img/series/4.png",
+        seriesName: "Jentelmens",
+        actor: "Iko Uwais",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/espn.png"
+    },
+    {
+        bgImgUrl: "../img/series/5.png",
+        seriesName: "Blade Runner",
+        actor: "Iko Uwais",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/usa.png"
+    },
+    {
+        bgImgUrl: "../img/series/6.png",
+        seriesName: "Wu Assassins",
+        actor: "Iko Uwais",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/trutv.png"
+    },
+    {
+        bgImgUrl: "../img/series/7.png",
+        seriesName: "Day Shift",
+        actor: "Iko Uwais",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/fx.png"
+    },
+    {
+        bgImgUrl: "../img/series/8.png",
+        seriesName: "Prisson",
+        actor: "Iko Uwais",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/bravo.png"
+    },
+    {
+        bgImgUrl: "../img/series/9.png",
+        seriesName: "Wu Assassins",
+        actor: "Iko Uwais",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/fox.png"
+    },
+    {
+        bgImgUrl: "../img/series/10.png",
+        seriesName: "Wu Assassins",
+        actor: "Iko Uwais",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/fox_news.png"
+    },
+    {
+        bgImgUrl: "../img/series/11.png",
+        seriesName: "Wu Assassins",
+        actor: "Iko Uwais",
+        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+        chanel: "../img/chanel_logo/fbs.png"
+    }
+];
+// var rec = document.querySelector('div[data-rec="rec"]')
+
+// function DslSeries(){
+//     var str =''
+//     for(i=0; i<5; i++){
+//         str+`
+//         <div class="series" style="background-image: url(${series[i].bgImgUrl});>
+//         <div class="name">
+//             <h4>Showing now</h4>
+//             <p>${series[i].seriesName}</p>
+//         </div>
+//         <div class="actors">
+//             <h4>Up Next</h4>
+//             <p>${series[i].actor}</p>
+//         </div>
+//         <div class="recens">
+//             <p>${series[i].recens}</p>
+//             <button>watch now</button>
+//         </div>
+//     </div>
+//         `
+//     }
+//     rec.innerHTML = str
+// }
+// DslSeries()
