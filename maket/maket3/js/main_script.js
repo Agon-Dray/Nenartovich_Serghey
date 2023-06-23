1f6e8007e557d605424cac130e161cae17669bce
AgonDray <argodray@gmail.com>
2023-06-23 21:20:31 +0300

    lessons

--- maket/maket3/js/main_script.js
+++ maket/maket3/js/main_script.js
@@ -41,48 +41,48 @@ var series = [
         recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
         chanel: "../img/chanel_logo/trutv.png"
     },
-    {
-        bgImgUrl: "../img/series/7.png",
-        seriesName: "Day Shift",
-        actor: "Iko Uwais",
-        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
-        chanel: "../img/chanel_logo/fx.png"
-    },
-    {
-        bgImgUrl: "../img/series/8.png",
-        seriesName: "Prisson",
-        actor: "Iko Uwais",
-        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
-        chanel: "../img/chanel_logo/bravo.png"
-    },
-    {
-        bgImgUrl: "../img/series/9.png",
-        seriesName: "Wu Assassins",
-        actor: "Iko Uwais",
-        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
-        chanel: "../img/chanel_logo/fox.png"
-    },
-    {
-        bgImgUrl: "../img/series/10.png",
-        seriesName: "Wu Assassins",
-        actor: "Iko Uwais",
-        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
-        chanel: "../img/chanel_logo/fox_news.png"
-    },
-    {
-        bgImgUrl: "../img/series/11.png",
-        seriesName: "Wu Assassins",
-        actor: "Iko Uwais",
-        recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
-        chanel: "../img/chanel_logo/fbs.png"
-    }
+    // {
+    //     bgImgUrl: "../img/series/7.png",
+    //     seriesName: "Day Shift",
+    //     actor: "Iko Uwais",
+    //     recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+    //     chanel: "../img/chanel_logo/fx.png"
+    // },
+    // {
+    //     bgImgUrl: "../img/series/8.png",
+    //     seriesName: "Prisson",
+    //     actor: "Iko Uwais",
+    //     recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+    //     chanel: "../img/chanel_logo/bravo.png"
+    // },
+    // {
+    //     bgImgUrl: "../img/series/9.png",
+    //     seriesName: "Wu Assassins",
+    //     actor: "Iko Uwais",
+    //     recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+    //     chanel: "../img/chanel_logo/fox.png"
+    // },
+    // {
+    //     bgImgUrl: "../img/series/10.png",
+    //     seriesName: "Wu Assassins",
+    //     actor: "Iko Uwais",
+    //     recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+    //     chanel: "../img/chanel_logo/fox_news.png"
+    // },
+    // {
+    //     bgImgUrl: "../img/series/11.png",
+    //     seriesName: "Wu Assassins",
+    //     actor: "Iko Uwais",
+    //     recens: "An unassuming San Francisco chef becomes the latest in a long line of assassins chosen to keep the mystical Wu powers out of the wrong hands.",
+    //     chanel: "../img/chanel_logo/fbs.png"
+    // }
 ];
 // var rec = document.querySelector('div[data-rec="rec"]')
 
 // function DslSeries(){
 //     var str =''
-//     for(i=0; i<5; i++){
-//         str+`
+//     for(var i=0; i<6; i++){
+//         str+=`
 //         <div class="series" style="background-image: url(${series[i].bgImgUrl});>
 //         <div class="name">
 //             <h4>Showing now</h4>
@@ -98,7 +98,9 @@ var series = [
 //         </div>
 //     </div>
 //         `
+
 //     }
+//     console.log(str)
 //     rec.innerHTML = str
 // }
 // DslSeries()
