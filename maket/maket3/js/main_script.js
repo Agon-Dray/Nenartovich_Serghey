a211a8d109be24d1f5a72a40eb54ed22f0117559
Agon-Dray <argodray@gmail.com>
2023-06-24 21:23:28 +0300

    abra_kodabra

--- maket/maket3/js/main_script.js
+++ maket/maket3/js/main_script.js
@@ -1,41 +1,42 @@
+
 var series = [
     {
-        bgImgUrl: "../img/series/1.png",
+        bgImgUrl: "../../../maket/maket3/img/series/1.png",
         seriesName: "Wu Assassins",
         actor: "Iko Uwais",
         recens: "Enjoy the best of action packed movies from FOX international...",
         chanel: "../img/chanel_logo/fox.png"
     },
     {
-        bgImgUrl: "../img/series/2.png",
+        bgImgUrl: "../../../maket/maket3/img/series/2.png",
         seriesName: "Project Adam",
         actor: "Iko Uwais",
         recens: "Enjoy the best of action packed movies from ABC international...",
         chanel: "../img/chanel_logo/abc.png"
     },
     {
-        bgImgUrl: "../img/series/3.png",
+        bgImgUrl: "../../../maket/maket3/img/series/3.png",
         seriesName: "Monster Truck",
         actor: "Emili Watson",
         recens: "Enjoy the best of action packed movies from NBC international...",        
         chanel: "../img/chanel_logo/nbc.png"
     },
     {
-        bgImgUrl: "../img/series/4.png",
+        bgImgUrl: "../../../maket/maket3/img/series/4.png",
         seriesName: "Jentelmens",
         actor: "Iko Uwais",
         recens: "Enjoy the best of action packed movies from ESPN international...",        
         chanel: "../img/chanel_logo/espn.png"
     },
     {
-        bgImgUrl: "../img/series/5.png",
+        bgImgUrl: "../../../maket/maket3/img/series/5.png",
         seriesName: "Blade Runner",
         actor: "Iko Uwais",
         recens: "Enjoy the best of action packed movies from USA international...",        
         chanel: "../img/chanel_logo/usa.png"
     },
     {
-        bgImgUrl: "../img/series/6.png",
+        bgImgUrl: "../../../maket/maket3/img/series/6.png",
         seriesName: "Wu Assassins",
         actor: "Iko Uwais",
         recens: "Enjoy the best of action packed movies from TruTV international...",        
@@ -44,30 +45,28 @@ var series = [
 ];
 var rec = document.querySelector('div[data-rec="rec"]')
 
-function DslSeries(){
-    var str =''
-    for(var i=0; i<series.length; i++){
-        str+=`
-    <div style="background: url(${series[i].bgImgUrl});">
-        <div class="series">
-            <div class="name">
-                <h4>Showing now</h4>
-                <p>${series[i].seriesName}</p>
+function DslSeries() {
+    var str = '';
+    for (var i = 0; i < series.length; i++) {
+        str += `
+    <div style="background-image: url(${series[i].bgImgUrl});">
+            <div class="series" >
+                <div class="name">
+                    <h4>Showing now</h4>
+                    <p>${series[i].seriesName}</p>
+                </div>
+                <div class="actors">
+                    <h4>Up Next</h4>
+                    <p>${series[i].actor}</p>
+                </div>
+                <div class="recens">
+                    <p>${series[i].recens}</p>
+                    <button><a href="#">Watch now</a></button>
+                </div>
             </div>
-            <div class="actors">
-                <h4>Up Next</h4>
-                <p>${series[i].actor}</p>
-            </div>
-            <div class="recens">
-                <p>${series[i].recens}</p>
-                <button>watch now</button>
-            </div>
-        </div>
-    </div>
-        `
-}
-    
-    rec.innerHTML = str
+        </div>`;
+    }
+    rec.innerHTML = str;
 }
 
 DslSeries()
---
+++ maket/maket3/js/movi_script.js
@@ -0,0 +1,75 @@
+
+// var movie = [
+//     {
+//         bgImgUrl: "../../../maket/maket3/img/movie/m1.png",
+//         movieName: "MovieName",
+//         recens: "serie and seasons",
+//     },
+//     {
+//         bgImgUrl: "../../../maket/maket3/img/movie/m2.png",
+//         movieName: "MovieName",
+//         recens: "serie and seasons",
+//     },
+//     {
+//         bgImgUrl: "../../../maket/maket3/img/movie/m3.png",
+//         movieName: "MovieName",
+//         recens: "serie and seasons",
+//     },
+//     {
+//         bgImgUrl: "../../../maket/maket3/img/movie/m4.png",
+//         movieName: "MovieName",
+//         recens: "serie and seasons",
+//     },
+//     {
+//         bgImgUrl: "../../../maket/maket3/img/movie/m5.png",
+//         movieName: "MovieName",
+//         recens: "serie and seasons",
+//     },
+//     {
+//         bgImgUrl: "../../../maket/maket3/img/movie/m6.png",
+//         movieName: "MovieName",
+//         recens: "serie and seasons",
+//     },
+//     {
+//         bgImgUrl: "../../../maket/maket3/img/movie/m7.png",
+//         movieName: "MovieName",
+//         recens: "serie and seasons",
+//     }   
+// ]
+// var rec = document.querySelector('div[data-movie="movie"]')
+
+// function DslMovie() {
+//     var str = '';
+//     for (var i = 0; i < movie.length; i++) {
+//         str += `
+//     <div class="movie" style="background-image: url(${movie[i].bgImgUrl});">
+//         <div class="recens">
+//             <h3>${movie[i].movieName}</h3>
+//             <p>${movie[i].recens}</p>
+//             <button></button>
+//         </div>
+//     </div>`;
+//     }
+//     rec.innerHTML = str;
+// }
+
+// DslMovie()
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 50,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 4
+            },
+            1000: {
+                items: 7
+            }
+        }
+    })
+});
