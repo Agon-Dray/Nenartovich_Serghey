8e5d89387713287ba359c8efb4311c48ea0baff6
Agon-Dray <argodray@gmail.com>
2023-06-25 20:48:44 +0300

    хз как успеть

--- maket/maket3/js/main_script.js
+++ maket/maket3/js/main_script.js
@@ -70,3 +70,20 @@ function DslSeries() {
 }
 
 DslSeries()
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
